bc31d57cfc5eaab93a5755da78db97f9cd5ab8f0
Sviatoslav <hacad@outlook.com>
2016-11-20 15:50:51 +0300

    Feedback form

---
+++ js/feedback.js
@@ -0,0 +1,20 @@
+var feedbackOverlay = document.querySelector(".feedback-overlay");
+var feedbackForm = document.querySelector(".feedback-form");
+
+var btnFeedbackShow = document.querySelector(".map-info .btn");
+var btnFeedbackClose = document.querySelector(".feedback-close");
+
+function showFeedbackForm(e) {
+  e.preventDefault();
+  feedbackOverlay.classList.add("feedback-overlay-show");
+  feedbackForm.classList.add("feedback-form-show");
+}
+
+function closeFeedbackForm(e) {
+    e.preventDefault();
+    feedbackOverlay.classList.remove("feedback-overlay-show");
+    feedbackForm.classList.remove("feedback-form-show");
+}
+
+btnFeedbackShow.addEventListener("click", showFeedbackForm);
+btnFeedbackClose.addEventListener("click", closeFeedbackForm);
